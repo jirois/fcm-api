4b0bbcfb90eba3ea0bed7ccf415fd9f939c11b23
Omas Ajiri <omasajiri@gmail.com>
2022-02-04 09:50:46 +0100

    fix: controller

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,11 @@
-const timeStampController = async (req, res) => {
-    const date = req.params.date
-    console.log(date);
+const timeStampController = (req, res) => {
+    const {date} = req.params 
+    if (!Number(date)){
+        res.json({error: "Invalid date"})
+    }
     const today = new Date(Number(date)).toUTCString()
     res.json({unix: date, utc: today})
+    
 }
 
 module.exports = timeStampController
